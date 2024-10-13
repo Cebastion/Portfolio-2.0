5230530afa402afc19b1ccb7ff613618194eabd2
Cebastion <kdimon2006@gmail.com>
2024-10-13 20:19:41 +0300

    asdasd

--- client/src/app/admin/page.tsx
+++ client/src/app/admin/page.tsx
@@ -48,7 +48,7 @@ const Page: FC = () => {
         formData.append('image', form.image);
       }
       console.log(formData)
-      const { data } = await axios.post<IWork>('https://server-jet-beta.vercel.app/works', formData)
+      const { data } = await axios.post<IWork>('https://server-five-mu-25.vercel.app/works', formData)
       return data
     } catch (error) {
       console.error(error)
