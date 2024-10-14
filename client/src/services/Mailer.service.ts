11ccd52643d6d22d5c0f0c74915902a6059396a3
Cebastion <kdimon2006@gmail.com>
2024-10-14 12:07:59 +0300

    SAS

--- client/src/services/Mailer.service.ts
+++ client/src/services/Mailer.service.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 export class MailerService {
   static async SendEmail(mailer: IMailer) {
     try {
-      await axios.post('https://server-alpha-tawny.vercel.app/sendEmail', mailer)
+      await axios.post('https://server-portfolio-sigma.vercel.app/sendEmail', mailer)
     } catch (error: any) {
       console.log(error)
     }
